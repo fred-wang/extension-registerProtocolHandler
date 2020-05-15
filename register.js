413ad967e3643990b83650501a62d7fdab7af109
Frédéric Wang <fwang@igalia.com>
2020-05-15 18:34:06 +0200

    Add a button to register with a user gesture.

--- register.js
+++ register.js
@@ -1,3 +1,10 @@
-console.log("Registering email and web+foo...");
-navigator.registerProtocolHandler("mailto", "./handler.html?protocol=%s", "title_mailto");
-navigator.registerProtocolHandler("web+foo", "./handler.html?protocol=%s", "title_webfoo");
+function register() {
+    console.log("Registering email and web+foo...");
+    navigator.registerProtocolHandler("mailto", "./handler.html?protocol=%s", "title_mailto");
+    navigator.registerProtocolHandler("web+foo", "./handler.html?protocol=%s", "title_webfoo");
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+    register(); // no user gesture
+    document.getElementsByTagName("button")[0].addEventListener("click", register); // user gesture
+});
